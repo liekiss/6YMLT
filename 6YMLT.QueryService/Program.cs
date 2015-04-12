4ba29f173573bb1e22123b5ad5054e85d28d88c6
PhantomGo <gfp88316@gmail.com>
2015-04-12 10:45:50 +0800

    initial solution

---
+++ 6YMLT.QueryService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6YMLT.QueryService {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
